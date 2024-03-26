084bb5dd3ab5a11407c409c57a953b28daaf4f04
Raphaël Badawi <raphael.badawi@ceo-vision.com>
2024-03-26 18:07:37 +0100

    send newsletters to all subscribers whenever a newsletter is published

--- back/src/api/newsletter/content-types/newsletter/lifecycles.ts
+++ back/src/api/newsletter/content-types/newsletter/lifecycles.ts
@@ -1,10 +1,19 @@
 export default {
-  afterUpdate(event) {
+  async afterUpdate(event) {
     const { result } = event;
     if (!result.publishedAt) {
       return;
     }
     // the newsletter was just published or was modified after publication
-    /** @todo finish nextjs api endpoint then make here a call to this api */
+    const res = await fetch(
+      `http://${process.env.NEXT_DOCKER_NETWORK_ENDPOINT}/api/newsletter`,
+      { 
+        method: "POST",
+        body: JSON.stringify({
+          jwt: process.env.ADMIN_JWT_SECRET,
+          content: result.content,
+        })
+      }
+  );
   },
 };
--- front/app/api/newsletter/route.ts
+++ front/app/api/newsletter/route.ts
@@ -1,6 +1,22 @@
+import { format } from "date-fns";
+import { fr } from "date-fns/locale";
 import getMultiple from "@/services/getMultiple";
+import sendMail from "@/services/sendMail";
 
 export async function POST(req: Request) {
   const subscribers = await getMultiple("subscribers");
-  /** @todo ADMIN_JWT_SECRET is expected in the request body for validation */
+  const body = await req.json();
+  if (body.jwt !== process.env.STRAPI_ADMIN_JWT) {
+    return new Response("KO", { status: 500 });
+  }
+  const date = format(new Date(), "d LLLL yyyy", { locale: fr });
+  for (const subscriber of subscribers.data) {
+    sendMail(
+      process.env.MAIL_USER || "",
+      subscriber.attributes.email,
+      `[FILLIÈRE TT] Newsletter du ${date}`,
+      body.content
+    );
+  }
+  return new Response("OK", { status: 200 });
 }
