1db738f395e94c6a2339fd38088597cd6a90cd8e
Raphaël Badawi <raphael.badawi@ceo-vision.com>
2024-07-08 14:24:58 +0200

    move coordinates to .env

--- front/app/practical/page.tsx
+++ front/app/practical/page.tsx
@@ -9,7 +9,7 @@ export default async function Practical() {
     return (
         <section id="practical" className="w-full">
             <SimpleHtmlContainer contentHtml={contentHtml} />
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2771.3550212325576!2d6.172277976800223!3d46.00410577108805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478b88f6300baec1%3A0x42e12aceb691cc5c!2sGymnase%20du%20Parmelan!5e0!3m2!1sfr!2sfr!4v1720439039821!5m2!1sfr!2sfr" width="600" height="450" className="border-0 mx-auto" allowFullScreen loading="lazy"></iframe>
+            <iframe src={process.env.NEXT_PUBLIC_MAPS_SRC} width="600" height="450" className="border-0 mx-auto" allowFullScreen loading="lazy"></iframe>
         </section>
 
     )
--- front/components/Footer.tsx
+++ front/components/Footer.tsx
@@ -6,12 +6,12 @@ export default function Footer() {
         <footer className="mt-4 relative">
             <div className="relative py-4 text-center bg-background z-10">
                 <NewsletterModal />
-                <address className="text-foreground not-italic">Gymnase du Parmelan, 74570 GROISY</address>
+                <address className="text-foreground not-italic">{process.env.NEXT_PUBLIC_ADDRESS}</address>
                 <div className="flex justify-center space-x-2">
-                    <a href="https://www.facebook.com/filliere.tennisdetable" className="text-foreground dark:text-gray-100 hover:text-gray-300 dark:hover:text-gray-500">
+                    <a href={process.env.NEXT_PUBLIC_FACEBOOK} className="text-foreground dark:text-gray-100 hover:text-gray-300 dark:hover:text-gray-500">
                         <FaFacebook className="h-6 w-6 rounded-full p-1 bg-white dark:bg-gray-700 shadow-lg" />
                     </a>
-                    <a href="https://www.instagram.com/fillierett/" className="text-foreground dark:text-gray-100 hover:text-gray-300 dark:hover:text-gray-500">
+                    <a href={process.env.NEXT_PUBLIC_INSTAGRAM} className="text-foreground dark:text-gray-100 hover:text-gray-300 dark:hover:text-gray-500">
                         <FaInstagram className="h-6 w-6 rounded-full p-1 bg-white dark:bg-gray-700 shadow-lg" />
                     </a>
                 </div>
