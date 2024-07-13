02e71c2206caa48b32575cbfc4471c53f102ab6a
Raphaël Badawi <raphael.badawi@ceo-vision.com>
2024-07-13 17:24:31 +0200

    escape single quotes for prod build

--- front/app/not-found.tsx
+++ front/app/not-found.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link'
 export default function NotFound() {
   return (
     <div className="w-full h-full flex flex-col justify-center items-center">
-      <h2>Cette table n'existe pas</h2>
-      <Link href="/" className="text-primary hover:underline">Retour à l'entrée du gymnase</Link>
+      <h2>Cette table n&apos;existe pas</h2>
+      <Link href="/" className="text-primary hover:underline">Retour à l&apos;entrée du gymnase</Link>
     </div>
   )
 }
