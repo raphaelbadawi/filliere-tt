9ea2b7c9dadd6c7712127e745dbfb3f57df83401
Raphaël Badawi <raphael.badawi@ceo-vision.com>
2024-09-25 11:49:52 +0200

    fix another linter regression

--- front/components/containers/ContestContainer.tsx
+++ front/components/containers/ContestContainer.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import ContestComponent from "../ContestComponent";
 
 export default function ContestContainer({ contests }: { contests: Contest[] }) {
-    const [targetContest, setTargetContest] = useState(0);
+    const [targetContest, setTargetContest] = useState("");
     const getContestFromTarget = () => {
         for (const contest of contests) {
             if (contest.documentId == targetContest) {
@@ -16,13 +16,13 @@ export default function ContestContainer({ contests }: { contests: Contest[] }) 
     }
     return (
         <>
-            <select defaultValue={0} onChange={e => setTargetContest(parseInt(e.target.value))} name="contest" className="w-full mt-4 px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary focus:border-primary dark:bg-gray-700 dark:border-gray-500 dark:placeholder-gray-300 dark:text-white dark:focus:ring-primary dark:focus:border-primary" id="contestSelect">
-                <option value={0} disabled>Choisissez une équipe</option>
+            <select defaultValue={""} onChange={e => setTargetContest(e.target.value)} name="contest" className="w-full mt-4 px-4 py-3 text-base text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-primary focus:border-primary dark:bg-gray-700 dark:border-gray-500 dark:placeholder-gray-300 dark:text-white dark:focus:ring-primary dark:focus:border-primary" id="contestSelect">
+                <option value={""} disabled>Choisissez une équipe</option>
                 {contests && contests.map((contest: Contest, index: number) => (
                     <option value={contest.documentId} key={index}>{contest.teamName}</option>
                 ))}
             </select>
-            {targetContest != 0 && (
+            {targetContest != "" && (
                 <ContestComponent contest={getContestFromTarget()} />
             )}
         </>
