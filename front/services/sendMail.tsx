0ab233904e785164ebab059705b7142e1d1e66a1
Raphaël Badawi <raphael.badawi@ceo-vision.com>
2025-02-19 19:41:42 +0100

    base64 encode mail creds in case pass has very special chars

--- front/services/sendMail.tsx
+++ front/services/sendMail.tsx
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
     auth: {
         user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS,
+        pass: atob(process.env.MAIL_PASS || ""),
     },
     secure: true,
 });
